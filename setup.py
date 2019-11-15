2b3f661a01f48dc1489e7447a34a599d839ab009
bbadzioch <badzioch@buffalo.edu>
2019-11-15 12:24:51 -0500

    changed version number to 1.7

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="ubgrade",
-    version="0.1.6",
+    version="0.1.7",
     description="Automates some tasks related to preparation and grading of exams.",
     long_description=README,
     long_description_content_type="text/markdown",
--- ubgrade/__init__.py
+++ ubgrade/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 
 from ubgrade.make_exams_tools import make_exams
